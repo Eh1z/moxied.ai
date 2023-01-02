7377251ae1371ef94c84587b5c2aeea255078c52
Eh1z <godsentehiz11@gmail.com>
2023-01-03 00:57:10 +0100

    configuring openai, express and cors

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/bog.svg';
+import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
@@ -32,3 +32,60 @@ function typeText(element, text) {
         }
     }, 20)
 }
+
+//Generate unique for each query ID
+function generateUniqueId() {
+    const timeStamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timeStamp}-${hexadecimalString};`
+}
+
+//Chat Block differentiation for AI and User
+function chatStripe (isAi, value, uniqueId) {
+    return (
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                <div class="chat">
+                    <div className="profile">
+                        <img
+                            src="${isAi ? bot : user}"
+                            alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+                    <div class="message" id=${uniqueId}>${value}></div>
+                </div>
+            </div>
+        `
+    )
+}
+
+//Submit Question Function
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    //user's chat stripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    form.reset();
+
+    //bot's chat stripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId);
+
+    loader(messageDiv)
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e);
+    }
+})
