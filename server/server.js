bacd06ebe58ed31a311705f6b0e0020e8a123e9c
Eh1z <godsentehiz11@gmail.com>
2023-01-05 18:37:30 +0100

    Fixed status code 401 issue

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(cors())
 app.use(express.json())
-/*
+
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from Godsent Ehiz!'
   })
 })
-*/
+
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
